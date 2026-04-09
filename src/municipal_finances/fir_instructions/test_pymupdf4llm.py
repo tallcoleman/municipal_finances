133e65e8b3bf99f623d261cd7591cf9c58f26966
Ben Coleman <ben.coleman@alum.utoronto.ca>
2026-04-09 11:52:35 -0400

    run pymupdf4llm on whole 2025 folder

--- src/municipal_finances/fir_instructions/test_pymupdf4llm.py
+++ src/municipal_finances/fir_instructions/test_pymupdf4llm.py
@@ -4,6 +4,12 @@ from typing import List
 import pymupdf4llm as md
 
 
+def convert_folder(path: Path):
+    pdf_paths = path.glob("*.pdf")
+    for pdf_path in pdf_paths:
+        convert_pdf(pdf_path)
+
+
 def convert_pdf(pdf_path: Path):
     result = md.to_markdown(
         pdf_path,
@@ -13,12 +19,10 @@ def convert_pdf(pdf_path: Path):
     )
     if isinstance(result, List):
         raise ValueError("wrong output format - list detected, should be string")
-    output_path = (
-        pdf_path.parent / "2025/pymupdf4llm" / pdf_path.with_suffix(".md").name
-    )
+    output_path = pdf_path.parent / "pymupdf4llm" / pdf_path.with_suffix(".md").name
     with output_path.open("w") as f:
         f.write(result)
 
 
 if __name__ == "__main__":
-    convert_pdf(Path("fir_instructions/source_files/FIR2025 Instructions.pdf"))
+    convert_folder(Path("fir_instructions/source_files/2025"))
